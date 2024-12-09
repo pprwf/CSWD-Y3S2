a6636f0ed527ad18792b89cf51808dad41d51dbe
Peerawich Pichayathadapong <pprwfwithsleeping.pills@gmail.com>
2024-12-09 15:53:43 +0700

    Lab - Test your skills: Variables | Week 03 (JavaScript)

---
+++ Week03/01_Variables.js
@@ -0,0 +1,16 @@
+//* Example 1
+
+let myName;
+myName = "Peerawich Pichayathadapong";
+let myAge = 21;
+
+//* Example 2
+
+let myName = "Paul";
+myName = "Peerawich Pichayathadapong";
+
+//* Example 3
+
+let myName = "Default";
+myName = "Chris";
+let myAge = 42;
